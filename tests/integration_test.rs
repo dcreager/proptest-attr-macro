5bf4d138a90985422a7bd8469f1039516f233af8
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2020-01-25 22:16:00 +0100

    Prove that functional strategies can have 2 arguments

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,17 +3,18 @@ use proptest_attr_macro::proptest;
 
 #[proptest(33u8..100u8)]
 fn inline_strategy(x: u8) {
-    assert!(x >= 33 && x < 100)
+    assert!(x >= 33 && x < 100);
 }
 
-#[proptest(up_to(43))]
+#[proptest(range(1, 5))]
 fn predefined_strategy(x: u8) {
-    assert!(x <= 42)
+    assert!(x >= 1 && x < 5);
 }
 
-#[proptest(up_to(43), up_to(100))]
+#[proptest(range(5, 7), range(10, 20))]
 fn multiple_strategies(x: u8, y: u8) {
-    assert!(x <= 42 && y < 100)
+    assert!(x >= 5 && x < 7);
+    assert!(y >= 10 && y < 20);
 }
 
 #[proptest(1u8..5u8, 6u8..10u8)]
@@ -23,8 +24,8 @@ fn multiple_inline_strategies(x: u8, y: u8) {
 }
 
 prop_compose! {
-  fn up_to(max_integer: u8)
-                       (integer in 0..max_integer)
+  fn range(from: u8, to: u8)
+                       (integer in from..to)
                        -> u8 {
     integer
   }
